11e0474f9f957215eaac1066b76572786f6d71b0
OliveMideva <67095878+olivemideva@users.noreply.github.com>
2022-04-21 21:50:41 +0300

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,61 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
-/**
- * _strlen - determines the string length
- * @str: given string
- *
- * Return: an integer length
- */
-int _strlen(const char *str)
-{
-	int length = 0;
-
-	while (str)
-		length++;
-	return (length);
-}
 
 /**
- * add_node_end - add a node at the end of list
- * @head: the head of the list
- * @str: a given string
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
  *
- * Return: a pointer to the new element
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *tmp;
-	tmp = *head;
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	if (!str)
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	/* nod memeorty allocation */
-	new_node = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
 
-	if (new_node == NULL)
-		return (NULL);
-	/* Nod creation and initialization */
-	new_node->next = NULL;
-	new_node->str = strdup(str);
-	/* check memory allocation for strdup */
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = _strlen(str);
-	/* checking if there is more than 2 elements(list exist) */
 	if (*head == NULL)
 	{
-		*head = new_node;
-		return (new_node);
+		*head = new;
+		return (new);
 	}
-	/* searcing for n-1 last element of the list where to point */
-	while (tmp->next)
-	{
-		tmp = tmp->next;
-	tmp->next = new_node;
-	}
-	return (new_node);
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
 }
