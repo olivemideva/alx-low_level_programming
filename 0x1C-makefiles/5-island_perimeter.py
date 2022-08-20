e7d6866307c34d39f406ac58eca09c98b017213b
Olive Mideva <67095878+olivemideva@users.noreply.github.com>
2022-08-20 13:08:23 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,12 @@ Island Perimeter
 
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid
-
     Args:
         grid(matrix): input grid
-
     Description:
         Traverse the land mass, and
         for the lake's nearest neighbor
         then return the total length
-
     """
     y = 0
     x = 0
@@ -30,4 +27,4 @@ def island_perimeter(grid):
                     if grid[row + 1][col] == 0:
                         x += 1
     return x + y
-    
+ 
